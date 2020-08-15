7e58fa5ddb0bf565d25f231cf71178dddeb47545
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2020-08-15 10:42:18 -0300

    Feita a vaidação e custom pipe

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v1 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -13,7 +13,7 @@ export class TasksService {
   }
 
   getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    const { status, seach } = filterDto;
+    const { status, search } = filterDto;
 
     let tasks = this.tasks;
 
@@ -21,9 +21,10 @@ export class TasksService {
       tasks = tasks.filter(task => task.status === status);
     }
 
-    if (seach) {
+    if (search) {
       tasks = tasks.filter(
-        task => task.title.includes(seach) || task.description.includes(seach),
+        task =>
+          task.title.includes(search) || task.description.includes(search),
       );
     }
 
@@ -31,7 +32,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const found = this.tasks.find(task => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return found;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -49,7 +56,8 @@ export class TasksService {
   }
 
   removeTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter(task => task.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
